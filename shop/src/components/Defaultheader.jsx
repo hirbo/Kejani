ac8f0988285b626bcc32bf5dd39c4f735b0a4b0d
hirbo <hirbobalasa@gmail.com>
2023-02-22 15:20:32 +0300

    dynamic header component

--- shop/src/components/Defaultheader.jsx
+++ shop/src/components/Defaultheader.jsx
@@ -1,44 +1,94 @@
-import React from 'react'
-import { NavLink, Outlet, useNavigate } from 'react-router-dom'
-import Logo from './logo.png'
-import { useLocation } from 'react-router-dom'
-import ForgotPassword from '../pages/Forgotpassword'
+import React, { useEffect, useState } from "react";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import Logo from "./logo.png";
+import { useLocation } from "react-router-dom";
+import ForgotPassword from "../pages/Forgotpassword";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function Header() {
-  const location = useLocation();
-  const matchroute = (route) => {
-    if (route === location.pathname) {
-      return true
-    }
+// Use the useLocation hook to get the current pathname
+const location = useLocation();
+
+// A function for checking the route matches the current pathname
+const matchroute = (route) => {  // Check if the route passed in matches the current pathname
+  if (route === location.pathname) {
+    return true;
   }
-  const navigate = useNavigate();
+};
+
+// Use the useState hook to set up the page state
+const [pageState, setPageState] = useState();
+
+// Get access to the navigate method from react-router-dom 
+const navigate = useNavigate();
+
+// Get the user's auth status
+const auth = getAuth();
+
+// Run a side effect whenever auth changes
+useEffect(() => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // Set the page state to profile if the user is authenticated
+      setPageState("profile");
+    } else {
+      // Else, the set the page state to signIn
+      setPageState("signIn");
+    }
+  });
+}, [auth]);
+
+
   return (
-    <div className=' bg-emerald-900  border-b shadow-sm sticky top-0 z-50 flex-wrap max-w-8xl mx-auto '>
-      <div className='   flex justify-between items-center'>
-        <div class=' px-3'>
-          <img src={Logo} alt="logo" class='h-24 w-32'  onClick={()=>navigate('/')}/>
+    <div className=" bg-emerald-900  border-b shadow-sm sticky top-0 z-50 flex-wrap max-w-8xl mx-auto ">
+      <div className="   flex justify-between items-center">
+        <div class=" px-3">
+          <img
+            src={Logo}
+            alt="logo"
+            class="h-24 w-32"
+            onClick={() => navigate("/")}
+          />
         </div>
 
-        <div className='space-x-10  mr-4 '>
-          
-        <NavLink to='/' className={`font-semibold py-3 text-lg border-b-4
-             border-b-transparent transition ease-in-out ${matchroute("/") && "text-black border-b-yellow-600" 
-              }`}>Home</NavLink>
-          <NavLink to='../pages/Offers' className={`font-semibold py-3 text-lg border-b-4 
-             border-b-transparent transition ease-in-out ${matchroute("/pages/Offers") && "text-black border-b-yellow-600 transition ease-in-out" 
-              }`}>Offers</NavLink>
-          
-          <NavLink to='../pages/signin' className={`font-semibold py-3 text-lg border-b-4
-             border-b-transparent transition ease-in-out ${matchroute("/pages/signin") && "text-black border-b-yellow-600  " 
-              }`}>sign IN</NavLink>
+        <div className="space-x-10  mr-4 ">
+          <NavLink
+            to="/"
+            onClick={() => navigate("/")}
+            className={`font-semibold py-3 text-lg border-b-4
+             border-b-transparent transition ease-in-out ${
+               matchroute("/") && "text-black border-b-yellow-600"
+             }`}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="../pages/Offers"
+            onClick={() => navigate("/pages/offers")}
+            className={`font-semibold py-3 text-lg border-b-4 
+             border-b-transparent transition ease-in-out ${
+               matchroute("/pages/Offers") &&
+               "text-black border-b-yellow-600 transition ease-in-out"
+             }`}
+          >
+            Offers
+          </NavLink>
 
+          <NavLink
+            to="../pages/signin"
+            className={`font-semibold py-3 text-lg border-b-4
+             border-b-transparent transition ease-in-out ${
+               (matchroute("/pages/signin") || matchroute("/pages/profile")) &&
+               "text-black border-b-yellow-600  "
+             }`}
+            onClick={() => navigate("/pages/signin")}
+          >
+            {pageState}
+          </NavLink>
         </div>
-
       </div>
-
-
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- shop/src/pages/Profile.jsx
+++ shop/src/pages/Profile.jsx
@@ -1,6 +1,9 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { db } from "../FirebaseConfig";
 
 function Profile() {
  //Get authentication and store as 'auth'
@@ -11,8 +14,8 @@ const User=auth.currentUser;
 
 // Create state to store user data (name, email)
 const [Data, setData] = useState({
-name:'SAMPLE',
-email:'SAMPLE'
+name:auth.currentUser.displayName,
+email:auth.currentUser.email
 });
 
 // Get values of name and email from Data
@@ -29,34 +32,81 @@ const navigate =useNavigate();
     auth.signOut();
     navigate('/')
   }
-
+  
+  // Setting a state variable called changed to false
+  const [changed, setChanged] = useState(false);
+  
+  // Defining a callback function that sets the data
+  function changeDetails(e) {
+    setData(prev => ({
+      ...prev,
+      [e.target.id]: e.target.value // Updating a single value in the data object
+    }));
+  }
+  
+  // Async function to update profile when the name has changed
+  async function submitForm() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // Getting authorization info
+        const auth = getAuth(); 
+  
+        // Updating the auth users' profile info
+        await updateProfile(auth.currentUser, { displayName: name });
+  
+        // Getting reference of the user from Firestore
+        const ref = doc(db, 'users', auth.currentUser.uid);
+  
+        // Updating the Firestore user's info 
+        await updateDoc(ref, {
+          name: name,
+        });
+  
+        // If successful, show a success toast message
+        toast.success('profile updated');
+      }
+    } catch(err) {
+      // Otherwise, show an error toast message
+      toast.err('error');
+    }
+  }
+  
   return (
     <section className=" max-w-6xl mx-auto justify-center items-center flex-col ">
       <h1 className=" text-center text-3xl font-bold font-serif ">profile</h1>
       <div className="w-full md:w-[50%cd shop] mt-6 px-3">
         <form action="">
           <input
+           onChange={changeDetails}
             type="text"
             id="name"
             value={name}
-            placeholder="name"
-            disabled
+            placeholder={name}
+            disabled ={!changed}
             className="w-full px-4 py-2 text-gray-700 bg-yellow-50 border-gray-900 rounded-md transition ease-in- text-lg font-sans "
           />
           <input
+          onChange={changeDetails}
             type="email"
             id="email"
             placeholder="email"
-            value={email}
-            disabled
-            className="w-full px-4 py-2 text-gray-700 bg-yellow-50 border-gray-900 rounded-md transition ease-in-out mt-8 text-lg "
+           value={email}
+            disabled={!changed}
+            className={`w-full px-4 py-2 text-gray-700 bg-yellow-50 border-gray-900 rounded-md transition ease-in-out mt-8 text-lg ${ 
+              changed && " bg-red-200 focus:bg-red-300 "}`}
           />
         </form>
         <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
           <p className="flex items-center text-gray-800 space-x-2 font-semibold ">
             Do you want to change your name?
-            <span className="text-red-600 hover:text-red-800 transition ease-in-out font-semibold cursor-pointer ">
-                Edit
+            <span 
+            onClick={()=>{ 
+              changed && submitForm()
+              setChanged(!changed)}
+            
+            }
+            className="text-red-600 hover:text-red-800 transition ease-in-out font-semibold cursor-pointer ">
+               {changed ? 'save changes':'Edit'}
             </span>
           </p>
           <p className="text-blue-600 hover:text-blue-800 transition
